f497e5d7f45dc2c0f1792e05af6f565403c15aeb
Wepeel <wepeelsuicune@gmail.com>
2020-11-30 17:13:22 +0200

    Fix Product

--- BE/Product.cs
+++ BE/Product.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Drawing;
 
 namespace BE
 {
@@ -8,10 +9,15 @@ namespace BE
     public class Product
     {
         private Enums.Category category;
+
         private Store store;
+
         private int price;
+
         public Enums.Category Category  { get; set; }
+
         public Store Store { get; set; }  
+
         public int Price { get; set ; }
     }
     public class Store
