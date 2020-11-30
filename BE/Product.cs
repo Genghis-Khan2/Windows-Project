609fdaf6f49163325e23f6511b936bc8356d730d
eithansegall <57600284+eithansegall@users.noreply.github.com>
2020-11-30 17:13:00 +0200

    classes

--- BE/Product.cs
+++ BE/Product.cs
@@ -4,25 +4,15 @@ using System.Text;
 
 namespace BE
 {
-
+    // gabi
     public class Product
     {
         private Enums.Category category;
         private Store store;
         private int price;
+
         public Enums.Category Category  { get; set; }
         public Store Store { get; set; }  
-        public int Price { get; set ; }
+        public int Price { get; set ; }   
     }
-    public class Store
-    {
-        
-    }
-    public class Costumer
-    {
-        string name;
-        string password;
-        public string Name { get; set; }
-        public string Password { get ; set; }      
-    }
-}
+  }
