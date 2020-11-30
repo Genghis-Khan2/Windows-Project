d745cc88c1f765a3053d82cde8b4626c531cae4c
Genghis Khan <gkomet@g.jct.ac.il>
2020-11-30 16:44:08 +0200

    Snuggles

---
+++ BE/Enums.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BE
+{
+    public class Enums
+    {
+        public enum Category { Food, Clothing, Comms, Weapons, Pets }
+    }
+}
---
+++ BE/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BE
+{
+
+    public class Product
+    {
+        private Enums.Category category;
+
+        public Enums.Category Category { get; set; }
+    }
+}
