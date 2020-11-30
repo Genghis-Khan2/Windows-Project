ae6132a06d9199163ea3c1a133c1302ed0794fe5
eithansegall <57600284+eithansegall@users.noreply.github.com>
2020-11-30 17:02:06 +0200

    product adding

--- BE/Product.cs
+++ BE/Product.cs
@@ -8,7 +8,21 @@ namespace BE
     public class Product
     {
         private Enums.Category category;
-
-        public Enums.Category Category { get; set; }
+        private Store store;
+        private int price;
+        public Enums.Category Category  { get; set; }
+        public Store Store { get; set; }  
+        public int Price { get; set ; }
+    }
+    public class Store
+    {
+        
+    }
+    public class Costumer
+    {
+        string name;
+        string password;
+        public string Name { get; set; }
+        public string Password { get ; set; }      
     }
 }
