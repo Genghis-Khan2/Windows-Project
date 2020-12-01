a99fc2dae606a62b30b3a9b7b20e96986f1425ab
Wepeel <wepeelsuicune@gmail.com>
2020-12-01 21:54:06 +0200

    Fixed BE properties

--- BE/Product.cs
+++ BE/Product.cs
@@ -8,12 +8,6 @@ namespace BE
 
     public class Product
     {
-        private Enums.Category category;
-
-        private Store store;
-
-        private int price;
-
         public Enums.Category Category  { get; set; }
 
         public Store Store { get; set; }
